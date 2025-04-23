0e333e0897eb4c5ea1723b3b019fabf40fb23d52
ONS <onsbenamara170@gmail.com>
2025-04-23 17:35:36 +0100

    upadated

---
+++ src/main/java/com/example/hackthon_iset_kr/HackthonIsetKrApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackthon_iset_kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackthonIsetKrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackthonIsetKrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackthon_iset_kr/HackthonIsetKrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackthon_iset_kr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackthonIsetKrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
